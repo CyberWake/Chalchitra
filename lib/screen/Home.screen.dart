2a9923162fd0df240b8ae85eadf52ab7471bec81
gautam <gk150899@gmail.com>
2020-08-02 14:17:31 +0530

    updated Home Screen

--- lib/screen/Home.screen.dart
+++ lib/screen/Home.screen.dart
@@ -1,23 +1,23 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class Home_Screen extends StatefulWidget {
-//   //Home_Screen({Key key}) : super(key: key);
+class Home_Screen extends StatefulWidget {
+  //Home_Screen({Key key}) : super(key: key);
 
-//   @override
-//   _Home_ScreenState createState() => _Home_ScreenState();
-// }
+  @override
+  _Home_ScreenState createState() => _Home_ScreenState();
+}
 
-// class _Home_ScreenState extends State<Home_Screen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: SafeArea(
-//       child: Container(
-//           height: double.infinity,
-//           decoration: BoxDecoration(
-//               image: DecorationImage(
-//                   image: AssetImage('assets/images/sign.png'),
-//                   fit: BoxFit.cover))),
-//     ));
-//   }
-// }
+class _Home_ScreenState extends State<Home_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+          height: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/sign.png'),
+                  fit: BoxFit.cover))),
+    ));
+  }
+}
