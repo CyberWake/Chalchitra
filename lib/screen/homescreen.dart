378268ea2facc4668245a6be3fe9f15905d42ca2
gautam <gk150899@gmail.com>
2020-08-14 10:05:58 +0530

    updated homepage

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -1,31 +1,138 @@
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:wowtalent/auth/auth_api.dart';
+
+// import 'package:wowtalent/notifier/auth_notifier.dart';
+
+// class HomeScreen extends StatelessWidget {
+//   // const Home_Screen({Key key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     AuthNotifier authNotifier = Provider.of<AuthNotifier>(context);
+
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//           title: Text(authNotifier.user != null
+//               ? authNotifier.user.displayName
+//               : "WowTalent"),
+//           backgroundColor: Colors.teal,
+//           elevation: 0.0,
+//           actions: <Widget>[
+//             FlatButton.icon(
+//               icon: Icon(Icons.person),
+//               label: Text('logout'),
+//               onPressed: () => signOut(authNotifier),
+//             )
+//           ]),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:wowtalent/auth/auth_api.dart';
+import 'package:wowtalent/data/post_json.dart';
+// import 'package:wowtalent/theme/colors.dart';
+import 'package:wowtalent/widgets/post_widget.dart';
 
-import 'package:wowtalent/notifier/auth_notifier.dart';
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-class HomeScreen extends StatelessWidget {
-  // const Home_Screen({Key key}) : super(key: key);
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    AuthNotifier authNotifier = Provider.of<AuthNotifier>(context);
+    return getBody();
+  }
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-          title: Text(authNotifier.user != null
-              ? authNotifier.user.displayName
-              : "WowTalent"),
-          backgroundColor: Colors.teal,
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('logout'),
-              onPressed: () => signOut(authNotifier),
-            )
-          ]),
+  Widget getBody() {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          // SingleChildScrollView(
+          //   scrollDirection: Axis.horizontal,
+          //   child: Row(
+          //     children: <Widget>[
+          //       Padding(
+          //         padding:
+          //             const EdgeInsets.only(right: 20, left: 15, bottom: 10),
+          //         child: Column(
+          //           children: <Widget>[
+          //             Container(
+          //               width: 65,
+          //               height: 65,
+          //               child: Stack(
+          //                 children: <Widget>[
+          //                   Container(
+          //                     width: 65,
+          //                     height: 65,
+          //                     decoration: BoxDecoration(
+          //                         shape: BoxShape.circle,
+          //                         image: DecorationImage(
+          //                             image: NetworkImage(profile),
+          //                             fit: BoxFit.cover)),
+          //                   ),
+          //                   Positioned(
+          //                       bottom: 0,
+          //                       right: 0,
+          //                       child: Container(
+          //                         width: 19,
+          //                         height: 19,
+          //                         decoration: BoxDecoration(
+          //                             shape: BoxShape.circle, color: black),
+          //                         child: Icon(
+          //                           Icons.add_circle,
+          //                           color: buttonFollowColor,
+          //                           size: 19,
+          //                         ),
+          //                       ))
+          //                 ],
+          //               ),
+          //             ),
+          //             SizedBox(
+          //               height: 8,
+          //             ),
+          //             SizedBox(
+          //               width: 70,
+          //               child: Text(
+          //                 name,
+          //                 overflow: TextOverflow.ellipsis,
+          //                 style: TextStyle(color: black),
+          //               ),
+          //             )
+          //           ],
+          //         ),
+          //       ),
+          //       Row(
+          //           children: List.generate(stories.length, (index) {
+          //         return StoryItem(
+          //           img: stories[index]['img'],
+          //           name: stories[index]['name'],
+          //         );
+          //       })),
+          //     ],
+          //   ),
+          // ),
+          // Divider(
+          //   color: black.withOpacity(0.3),
+          // ),
+          Column(
+            children: List.generate(posts.length, (index) {
+              return PostItem(
+                postImg: posts[index]['postImg'],
+                profileImg: posts[index]['profileImg'],
+                name: posts[index]['name'],
+                caption: posts[index]['caption'],
+                isLoved: posts[index]['isLoved'],
+                viewCount: posts[index]['commentCount'],
+                likedBy: posts[index]['likedBy'],
+                dayAgo: posts[index]['dayAgo'],
+              );
+            }),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/screen/profileScreen.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wowtalent/auth/auth_api.dart';
+import 'package:wowtalent/notifier/auth_notifier.dart';
+
+class ProfilPage extends StatefulWidget {
+  final String url =
+      "https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=1260";
+
+  // ProfilPage({@required this.url});
+  @override
+  _ProfilPageState createState() => _ProfilPageState();
+}
+
+class _ProfilPageState extends State<ProfilPage> {
+  @override
+  Widget build(BuildContext context) {
+    AuthNotifier authNotifier = Provider.of<AuthNotifier>(context);
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 35),
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Icon(Icons.arrow_back_ios)),
+                FlatButton.icon(
+                  onPressed: () => signOut(authNotifier),
+                  label: Text('LogOut'),
+                  icon: Icon(
+                    Icons.face,
+                    color: Colors.black,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Hero(
+            tag: widget.url,
+            child: Container(
+              margin: EdgeInsets.only(top: 35),
+              height: 80,
+              width: 80,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 5,
+                    blurRadius: 20,
+                  )
+                ],
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(widget.url),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            authNotifier.user != null
+                ? authNotifier.user.displayName
+                : "WowTalent",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "London, England",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[400],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              buildStatColumn("53", "Photos"),
+              buildStatColumn("223k", "Followers"),
+              buildStatColumn("117", "Following"),
+            ],
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(left: 8, right: 8, top: 8),
+              decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.15),
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(25))),
+              child: GridView.count(
+                padding: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                crossAxisCount: 2,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+                childAspectRatio: 5 / 6,
+                children: [
+                  buildPictureCard(
+                      "https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=1260"),
+                  buildPictureCard(
+                      "https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=1260"),
+                  buildPictureCard(
+                      "https://images.pexels.com/photos/733475/pexels-photo-733475.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=1260"),
+                  buildPictureCard(
+                      "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=1260"),
+                  buildPictureCard(
+                      "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=1260"),
+                  buildPictureCard(
+                      "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=1260"),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Card buildPictureCard(String url) {
+    return Card(
+      elevation: 10,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(url),
+            )),
+      ),
+    );
+  }
+
+  Column buildStatColumn(String value, String title) {
+    return Column(
+      children: [
+        Text(
+          value,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[400],
+          ),
+        ),
+      ],
+    );
+  }
+}
