c637e98807b54f382f4fe0ddf8adf9952a6692c5
CyberWake <ritikfbd@gmail.com>
2020-09-13 18:30:01 +0530

    Added a new video player

--- lib/screen/mainScreens/uploadVideo/video_uploader_widget/player.dart
+++ lib/screen/mainScreens/uploadVideo/video_uploader_widget/player.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -182,7 +183,7 @@ class _FadeAnimationState extends State<FadeAnimation>
   void initState() {
     super.initState();
     animationController =
-        AnimationController(duration: widget.duration, vsync: this);
+        AnimationController(duration: widget.duration, value: this);
     animationController.addListener(() {
       if (mounted) {
         setState(() {});
@@ -345,5 +346,226 @@ class AspectRatioVideoState extends State<AspectRatioVideo> {
     } else {
       return Center(child: CircularProgressIndicator());
     }
+  }
+}
+*/
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter/material.dart';
+import 'package:wowtalent/database/firebase_provider.dart';
+import 'package:wowtalent/model/video_info.dart';
+import 'package:wowtalent/screen/mainScreens/home/comments.dart';
+
+class Player extends StatefulWidget {
+  final VideoInfo video;
+  Player({Key key, @required this.video}) : super(key: key);
+  @override
+  _PlayerState createState() => _PlayerState();
+}
+
+class _PlayerState extends State<Player> {
+  VideoPlayerController _controller;
+  double _widthOne;
+  double _fontOne;
+  double _iconOne;
+  Size _size;
+  double _sliderValue = 0.0;
+  UserVideoStore _userVideoStore = UserVideoStore();
+  int likeCount = 0;
+  int commentCount = 0;
+  bool _isLiked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.video.videoUrl)
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _size = MediaQuery.of(context).size;
+    _widthOne = _size.width * 0.0008;
+    _fontOne = (_size.height * 0.015) / 11;
+    _iconOne = (_size.height * 0.066) / 50;
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: _controller.value.initialized
+          ? Container(
+        color: Colors.black,
+            child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.75,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: NetworkImage('https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=1260'),
+                          radius: 13,
+                        ),
+                        Text('  USERNAME \u2022',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+                        GestureDetector(
+                          onTap:(){
+
+                          },
+                          child: Text(' Follow',style: TextStyle(color: Colors.white),),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 23,top: 5),
+                    child: Row(
+                      children: [
+                        Icon(Icons.equalizer,color: Colors.white,),
+                        Text('  TITLE \u2022',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+                        Text(' Category',style: TextStyle(color: Colors.white),)
+                      ],
+                    ),
+                  ),
+                  /*FloatingActionButton(
+                    backgroundColor: Colors.grey,
+                    elevation: 0.0,
+                    onPressed: () {
+                      setState(() {
+                        _controller.value.isPlaying
+                            ? _controller.pause()
+                            : _controller.play();
+                      });
+                    },
+                    child: Icon(
+                      _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                      color: Colors.white,
+                    ),
+                  ),*/
+                  Container(
+                    color: Colors.black12.withOpacity(0.4),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            InkWell(
+                                child: SvgPicture.asset(
+                                  _isLiked?"assets/images/loved_icon.svg":"assets/images/love_icon.svg",
+                                  color: Colors.white,
+                                  width: 20,
+                                ),
+                                onTap: () async {
+                                  setState(() {
+                                    _isLiked = _isLiked == true ? false:true;
+                                    likeCount = _isLiked?likeCount+1:likeCount-1;
+                                  });
+                                }
+                            ),
+                            SizedBox(width: _widthOne * 20,),
+                            Text(
+                              likeCount.toString(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: _fontOne * 14,
+                                  color: Colors.white
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: _widthOne * 30,),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              onPressed: (){
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => CommentsScreen(
+                                          videoId: widget.video.uploaderUid,
+                                        )
+                                    )
+                                );
+                              },
+                              icon: Icon(
+                                Icons.comment,
+                                color: Colors.white,
+                                size: _iconOne * 23,
+                              ),
+                            ),
+                            SizedBox(width: _widthOne * 20,),
+                            Text(
+                              commentCount.toString(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: _fontOne * 14,
+                                  color: Colors.white
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: _widthOne * 30,),
+                        SizedBox(
+                          width: _widthOne * 650,
+                          child: Slider(
+                            value: _sliderValue,
+                            min: 0,
+                            max: 5,
+                            onChangeEnd: (val) async {
+                              _sliderValue = val;
+                              bool success =
+                              await _userVideoStore.rateVideo(videoID:widget.video.uploaderUid,rating: _sliderValue);
+                              if(success){
+                                print('done rating');
+                              }
+                              else{
+                                print('failure');
+                              }
+                            },
+                            onChanged: (val) {
+                              setState(() {
+                                _sliderValue = val;
+                              });
+                            },
+                            inactiveColor: Colors.white,
+                            activeColor: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+            ]
+      ),
+          )
+          : Container(),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
